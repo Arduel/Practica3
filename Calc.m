f5c0b0a1803ba46a0cc074b4864562695ed0a462
Christian_Arduel <cacb97arduel@gmail.com>
2021-05-31 00:52:19 -0500

    Se modifica Calc.m por implementacion en GUIDE

--- Calc.m
+++ Calc.m
@@ -1,7 +1,7 @@
-function [Vol,A,dx,dy,Px,Py,X,Y,Z] = Calc(F,x,y,xdiv,ydiv)
+function [Vol,A,dx,dy,Px,Py,X,Y,Z] = Calc(F,Xx,Yy,xdiv,ydiv)
 % Funcion que realiza los calculos correspondientes del volumen y los
 % puntos considerados para el calculo
-
+    syms x y
     Fxy(x,y)=F;
 
     %Calculo de puntos
@@ -9,8 +9,8 @@ function [Vol,A,dx,dy,Px,Py,X,Y,Z] = Calc(F,x,y,xdiv,ydiv)
     Py=zeros(xdiv,ydiv);
     for i=1:1:xdiv
         for j=1:1:ydiv
-            Px(i,j)= x(i) + (x(i+1)-x(i)).*rand(1,1); %Puntos en x
-            Py(i,j)= y(j) + (y(j+1)-y(j)).*rand(1,1); %Puntos en y
+            Px(i,j)= Xx(i) + (Xx(i+1)-Xx(i)).*rand(1,1); %Puntos en x
+            Py(i,j)= Yy(j) + (Yy(j+1)-Yy(j)).*rand(1,1); %Puntos en y
         end
     end
      
@@ -22,27 +22,27 @@ function [Vol,A,dx,dy,Px,Py,X,Y,Z] = Calc(F,x,y,xdiv,ydiv)
         end
     end
     
-    dx = diff([x(1) x(2)]); %Base de región
-    dy = diff([y(1) y(2)]); %Largo de región
+    dx = diff([Xx(1) Xx(2)]); %Base de región
+    dy = diff([Yy(1) Yy(2)]); %Largo de región
     A = dx.*dy; %Área de la región 
     Vol = s.*A; %Volumen aproximado bajo la función
     
-    if size(x,2) == size(y,2)
-        n=size(x,2);
-    elseif size(x,2) > size(y,2)
-        n=size(x,2);
+    if size(Xx,2) == size(Yy,2)
+        n=size(Xx,2);
+    elseif size(Xx,2) > size(Yy,2)
+        n=size(Xx,2);
         aux=zeros(1,n);
-        aux(1,1:n)=min(y);
-        aux(1,1:size(y,2))=y;
-        y=aux;
-    elseif size(y,2) > size(x,2)
-        n=size(y,2);
+        aux(1,1:n)=min(Yy);
+        aux(1,1:size(Yy,2))=Yy;
+        Yy=aux;
+    elseif size(Yy,2) > size(Xx,2)
+        n=size(Yy,2);
         aux=zeros(1,n);
-        aux(1,1:n)=min(x);
-        aux(1,1:size(x,2))=x;
-        x=aux;
+        aux(1,1:n)=min(Xx);
+        aux(1,1:size(Xx,2))=Xx;
+        Xx=aux;
     end
-    [X,Y] = meshgrid(x,y);
+    [X,Y] = meshgrid(Xx,Yy);
     Z = zeros(n);
 
 end
