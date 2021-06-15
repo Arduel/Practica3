64166fbabd3c3a159302bda8e03b1b3143610375
Christian_Arduel <cacb97arduel@gmail.com>
2021-06-15 09:38:08 -0500

    Se implemento función de no gráficar sin datos

--- Practica_3.m
+++ Practica_3.m
@@ -67,6 +67,8 @@ ax.XColor = [1 0.27 0];
 ax.YColor = [1 0.27 0];
 ax.ZColor = [1 0.27 0];
 
+set(handles.CB_C,'Value',1);
+set(handles.CB_P,'Value',1);
 
 handles.output = hObject;
 
@@ -84,91 +86,102 @@ varargout{1} = handles.output;
 % --- Executes on button press in B_G.
 function B_G_Callback(hObject, eventdata, handles)
 global PPx PPy Dx Dy Cc Ff Cx Cy Cz
-
 syms x y
-P=get(handles.CB_P,'Value');
-Cn=get(handles.CB_C,'Value');
-F_xy=str2sym(get(handles.Fxy,'String'));
-Xmin=str2double(get(handles.xmin,'String'));
-Xmax=str2double(get(handles.xmax,'String'));
-Xdiv=str2double(get(handles.xdiv,'String'));
-Ymin=str2double(get(handles.ymin,'String'));
-Ymax=str2double(get(handles.ymax,'String'));
-Ydiv=str2double(get(handles.ydiv,'String'));
 
-Xx=linspace(Xmin,Xmax,Xdiv+1);
-Yy=linspace(Ymin,Ymax,Ydiv+1);
+F_xy=size(get(handles.Fxy,'String'),2);
+Xmin=size(get(handles.xmin,'String'),2);
+Xmax=size(get(handles.xmax,'String'),2);
+Ymin=size(get(handles.ymin,'String'),2);
+Ymax=size(get(handles.ymax,'String'),2);
 
-[Vol,A,dx,dy,Px,Py,X,Y,Z,C,F] = Calc(F_xy,Xx,Yy,Xdiv,Ydiv);
+if F_xy ~= 0 && Xmin ~= 0 && Xmax ~= 0 && Ymin ~= 0 && Ymax~=0
+    P=get(handles.CB_P,'Value');
+    Cn=get(handles.CB_C,'Value');
+    F_xy=str2sym(get(handles.Fxy,'String'));
+    Xmin=str2double(get(handles.xmin,'String'));
+    Xmax=str2double(get(handles.xmax,'String'));
+    Xdiv=str2double(get(handles.xdiv,'String'));
+    Ymin=str2double(get(handles.ymin,'String'));
+    Ymax=str2double(get(handles.ymax,'String'));
+    Ydiv=str2double(get(handles.ydiv,'String'));
 
-PPx=Px;
-PPy=Py;
-Dx=dx;
-Dy=dy;
-Cx=X;
-Cy=Y;
-Cz=Z;
-Cc=C;
-Ff=F;
+    Xx=linspace(Xmin,Xmax,Xdiv+1);
+    Yy=linspace(Ymin,Ymax,Ydiv+1);
 
-Reg=1;
+    [Vol,A,dx,dy,Px,Py,X,Y,Z,C,F] = Calc(F_xy,Xx,Yy,Xdiv,Ydiv);
 
-Fxy(x,y)=F_xy;
-F_Ev=char(vpa(Fxy(PPx(Reg),PPy(Reg))));
+    PPx=Px;
+    PPy=Py;
+    Dx=dx;
+    Dy=dy;
+    Cx=X;
+    Cy=Y;
+    Cz=Z;
+    Cc=C;
+    Ff=F;
 
-set(handles.Vol,'String',char(Vol))
-set(handles.DX,'String',num2str(dx))
-set(handles.DY,'String',num2str(dy))
-set(handles.A,'String',num2str(A))
-set(handles.Reg,'String',num2str(Reg))
-set(handles.VolP,'String',char(abs(vpa(Fxy(Px(Reg),Py(Reg))))*A))
+    Reg=1;
 
-%Grafica 1
-axes(handles.g_fxy);
-ax = gca;   
-cla
-grid on
-hold on
-mesh(Cx,Cy,Cz)
-fmesh(F_xy,[Xmin Xmax Ymin Ymax])
-if P == 1
-    plot(Px,Py,'r.','MarkerSize',10)
+    Fxy(x,y)=F_xy;
+    F_Ev=char(vpa(Fxy(PPx(Reg),PPy(Reg))));
+
+    set(handles.Vol,'String',char(Vol))
+    set(handles.DX,'String',num2str(dx))
+    set(handles.DY,'String',num2str(dy))
+    set(handles.A,'String',num2str(A))
+    set(handles.Reg,'String',num2str(Reg))
+    set(handles.VolP,'String',char(abs(vpa(Fxy(Px(Reg),Py(Reg))))*A))
+
+    %Grafica 1
+    axes(handles.g_fxy);
+    ax = gca;   
+    cla
+    grid on
+    hold on
+    mesh(Cx,Cy,Cz)
+    fmesh(F_xy,[Xmin Xmax Ymin Ymax])
+    if P == 1
+        plot(Px,Py,'r.','MarkerSize',10)
+    end
+    if Cn == 1
+        fcontour(F_xy,[Xmin Xmax Ymin Ymax])
+    end
+
+    %Grafica 2
+    axes(handles.g_cu);
+    ax = gca;   
+    cla
+    hold on
+    mesh(Cx,Cy,Cz)
+    if P == 1
+        plot(Px,Py,'r.','MarkerSize',10)
+    end
+    if Cn == 1
+        fcontour(F_xy,[Xmin Xmax Ymin Ymax])
+    end
+
+    %Grafica 3
+    axes(handles.g_zoom)
+    ax = gca;   
+    cla
+    axis([Cc(Reg) Cc(Reg)+Dx Ff(Reg) Ff(Reg)+Dy]);
+    hold on
+    mesh(Cx,Cy,Cz)
+    plot(Px,Py,'r.','MarkerSize',30)
+    if Cn == 1
+        fcontour(F_xy,[Xmin Xmax Ymin Ymax])
+    end
+    set(handles.CPx,'String',mat2str(round(PPx(Reg),6,'significant')))
+    set(handles.CPy,'String',mat2str(round(PPy(Reg),6,'significant')))
+    set(handles.FEval,'String',F_Ev)
+else
+    disp("No hay datos")
 end
-if Cn == 1
-    fcontour(F_xy,[Xmin Xmax Ymin Ymax])
-end
-
-%Grafica 2
-axes(handles.g_cu);
-ax = gca;   
-cla
-hold on
-mesh(Cx,Cy,Cz)
-if P == 1
-    plot(Px,Py,'r.','MarkerSize',10)
-end
-if Cn == 1
-    fcontour(F_xy,[Xmin Xmax Ymin Ymax])
-end
-
-%Grafica 3
-axes(handles.g_zoom)
-ax = gca;   
-cla
-axis([Cc(Reg) Cc(Reg)+Dx Ff(Reg) Ff(Reg)+Dy]);
-hold on
-mesh(Cx,Cy,Cz)
-plot(Px,Py,'r.','MarkerSize',30)
-if Cn == 1
-    fcontour(F_xy,[Xmin Xmax Ymin Ymax])
-end
-set(handles.CPx,'String',mat2str(round(PPx(Reg),6,'significant')))
-set(handles.CPy,'String',mat2str(round(PPy(Reg),6,'significant')))
-set(handles.FEval,'String',F_Ev)
-
 
 % --- Executes on button press in B_L.
 function B_L_Callback(hObject, eventdata, handles)
+global PPx PPy Dx Dy Cc Ff Cx Cy Cz
+
 set(handles.A,'String','')
 set(handles.DX,'String','')
 set(handles.DY,'String','')
@@ -192,134 +205,175 @@ cla
 axes(handles.g_zoom)
 cla
 
+PPx=0;
+PPy=0;
+Dx=0;
+Dy=0;
+Cx=0;
+Cy=0;
+Cz=0;
+Cc=0;
+Ff=0;
+
 % --- Executes on button press in B_Ls.
 function B_Ls_Callback(hObject, eventdata, handles)
 global PPx PPy Dx Dy Cc Ff Cx Cy Cz
 syms x y
-Cn=get(handles.CB_C,'Value');
-F_xy=str2sym(get(handles.Fxy,'String'));
-Xmin=str2double(get(handles.xmin,'String'));
-Xmax=str2double(get(handles.xmax,'String'));
-Ymin=str2double(get(handles.ymin,'String'));
-Ymax=str2double(get(handles.ymax,'String'));
-Reg=str2double(get(handles.Reg,'String'));
-Px=transpose(PPx);
-Py=transpose(PPy);
-Fxy(x,y)=F_xy;
-if Reg > 1
-    Reg=Reg-1;
-    set(handles.Reg,'String',num2str(Reg))
-end
-F_Ev=char(vpa(Fxy(PPx(Reg),PPy(Reg))));
-axes(handles.g_zoom);
-ax = gca;   
-cla
-axis([Cc(Reg) Cc(Reg)+Dx Ff(Reg) Ff(Reg)+Dy]);
-hold on
-mesh(Cx,Cy,Cz)
-plot(Px,Py,'r.','MarkerSize',30)
-if Cn == 1
-    fcontour(F_xy,[Xmin Xmax Ymin Ymax])
-end
-set(handles.CPx,'String',mat2str(round(PPx(Reg),6,'significant')))
-set(handles.CPy,'String',mat2str(round(PPy(Reg),6,'significant')))
-set(handles.FEval,'String',F_Ev)
-set(handles.VolP,'String',char(abs(vpa(Fxy(PPx(Reg),PPy(Reg))))*Dx*Dy))
 
+F_xy=size(get(handles.Fxy,'String'),2);
+Xmin=size(get(handles.xmin,'String'),2);
+Xmax=size(get(handles.xmax,'String'),2);
+Ymin=size(get(handles.ymin,'String'),2);
+Ymax=size(get(handles.ymax,'String'),2);
+
+if F_xy ~= 0 && Xmin ~= 0 && Xmax ~= 0 && Ymin ~= 0 && Ymax~=0
+    Cn=get(handles.CB_C,'Value');
+    F_xy=str2sym(get(handles.Fxy,'String'));
+    Xmin=str2double(get(handles.xmin,'String'));
+    Xmax=str2double(get(handles.xmax,'String'));
+    Ymin=str2double(get(handles.ymin,'String'));
+    Ymax=str2double(get(handles.ymax,'String'));
+    Reg=str2double(get(handles.Reg,'String'));
+    Px=transpose(PPx);
+    Py=transpose(PPy);
+    Fxy(x,y)=F_xy;
+    if Reg > 1
+        Reg=Reg-1;
+        set(handles.Reg,'String',num2str(Reg))
+    end
+    F_Ev=char(vpa(Fxy(PPx(Reg),PPy(Reg))));
+    axes(handles.g_zoom);
+    ax = gca;   
+    cla
+    axis([Cc(Reg) Cc(Reg)+Dx Ff(Reg) Ff(Reg)+Dy]);
+    hold on
+    mesh(Cx,Cy,Cz)
+    plot(Px,Py,'r.','MarkerSize',30)
+    if Cn == 1
+        fcontour(F_xy,[Xmin Xmax Ymin Ymax])
+    end
+    set(handles.CPx,'String',mat2str(round(PPx(Reg),6,'significant')))
+    set(handles.CPy,'String',mat2str(round(PPy(Reg),6,'significant')))
+    set(handles.FEval,'String',F_Ev)
+    set(handles.VolP,'String',char(abs(vpa(Fxy(PPx(Reg),PPy(Reg))))*Dx*Dy))
+else
+    disp("No hay datos")
+end
 
 
 % --- Executes on button press in B_Ps.
 function B_Ps_Callback(hObject, eventdata, handles)
 global PPx PPy Dx Dy Cc Ff Cx Cy Cz
 syms x y
-Cn=get(handles.CB_C,'Value');
-F_xy=str2sym(get(handles.Fxy,'String'));
-Xmin=str2double(get(handles.xmin,'String'));
-Xmax=str2double(get(handles.xmax,'String'));
-Ymin=str2double(get(handles.ymin,'String'));
-Ymax=str2double(get(handles.ymax,'String'));
-xd=str2double(get(handles.xdiv,'String'));
-yd=str2double(get(handles.ydiv,'String'));
-Reg=str2double(get(handles.Reg,'String'));
-Px=transpose(PPx);
-Py=transpose(PPy);
-Fxy(x,y)=F_xy;
-if Reg < xd*yd
-    Reg=Reg+1;
-    set(handles.Reg,'String',num2str(Reg))
-end
-F_Ev=char(vpa(Fxy(PPx(Reg),PPy(Reg))));
-axes(handles.g_zoom);
-ax = gca;   
-cla
-mesh(Cx,Cy,Cz)
-axis([Cc(Reg) Cc(Reg)+Dx Ff(Reg) Ff(Reg)+Dy]);
-hold on
-plot(Px,Py,'r.','MarkerSize',30)
-if Cn == 1
-    fcontour(F_xy,[Xmin Xmax Ymin Ymax])
-end
-set(handles.CPx,'String',mat2str(round(PPx(Reg),6,'significant')))
-set(handles.CPy,'String',mat2str(round(PPy(Reg),6,'significant')))
-set(handles.FEval,'String',F_Ev)
-set(handles.VolP,'String',char(abs(vpa(Fxy(PPx(Reg),PPy(Reg))))*Dx*Dy))
 
+F_xy=size(get(handles.Fxy,'String'),2);
+Xmin=size(get(handles.xmin,'String'),2);
+Xmax=size(get(handles.xmax,'String'),2);
+Ymin=size(get(handles.ymin,'String'),2);
+Ymax=size(get(handles.ymax,'String'),2);
+
+if F_xy ~= 0 && Xmin ~= 0 && Xmax ~= 0 && Ymin ~= 0 && Ymax~=0
+    Cn=get(handles.CB_C,'Value');
+    F_xy=str2sym(get(handles.Fxy,'String'));
+    Xmin=str2double(get(handles.xmin,'String'));
+    Xmax=str2double(get(handles.xmax,'String'));
+    Ymin=str2double(get(handles.ymin,'String'));
+    Ymax=str2double(get(handles.ymax,'String'));
+    xd=str2double(get(handles.xdiv,'String'));
+    yd=str2double(get(handles.ydiv,'String'));
+    Reg=str2double(get(handles.Reg,'String'));
+    Px=transpose(PPx);
+    Py=transpose(PPy);
+    Fxy(x,y)=F_xy;
+    if Reg < xd*yd
+        Reg=Reg+1;
+        set(handles.Reg,'String',num2str(Reg))
+    end
+    F_Ev=char(vpa(Fxy(PPx(Reg),PPy(Reg))));
+    axes(handles.g_zoom);
+    ax = gca;   
+    cla
+    mesh(Cx,Cy,Cz)
+    axis([Cc(Reg) Cc(Reg)+Dx Ff(Reg) Ff(Reg)+Dy]);
+    hold on
+    plot(Px,Py,'r.','MarkerSize',30)
+    if Cn == 1
+        fcontour(F_xy,[Xmin Xmax Ymin Ymax])
+    end
+    set(handles.CPx,'String',mat2str(round(PPx(Reg),6,'significant')))
+    set(handles.CPy,'String',mat2str(round(PPy(Reg),6,'significant')))
+    set(handles.FEval,'String',F_Ev)
+    set(handles.VolP,'String',char(abs(vpa(Fxy(PPx(Reg),PPy(Reg))))*Dx*Dy))
+else
+    disp("No hay datos")
+end
 
 % --- Executes on button press in CB_P.
 function CB_P_Callback(hObject, eventdata, handles)
 global PPx PPy Dx Dy Cx Cy Cz Cc Ff
 syms x y
-Cn=get(handles.CB_C,'Value');
-F_xy=str2sym(get(handles.Fxy,'String'));
-Xmin=str2double(get(handles.xmin,'String'));
-Xmax=str2double(get(handles.xmax,'String'));
-Ymin=str2double(get(handles.ymin,'String'));
-Ymax=str2double(get(handles.ymax,'String'));
-xd=str2double(get(handles.xdiv,'String'));
-yd=str2double(get(handles.ydiv,'String'));
-Reg=str2double(get(handles.Reg,'String'));
-Px=transpose(PPx);
-Py=transpose(PPy);
-Fxy(x,y)=F_xy;
 
-%Grafica 1
-axes(handles.g_fxy);
-ax = gca;   
-cla
-grid on
-hold on
-mesh(Cx,Cy,Cz)
-fmesh(F_xy,[Xmin Xmax Ymin Ymax])
-if hObject.Value == 1
-    plot(Px,Py,'r.','MarkerSize',10)
-end
-if Cn == 1
-    fcontour(F_xy,[Xmin Xmax Ymin Ymax])
-end
+F_xy=size(get(handles.Fxy,'String'),2);
+Xmin=size(get(handles.xmin,'String'),2);
+Xmax=size(get(handles.xmax,'String'),2);
+Ymin=size(get(handles.ymin,'String'),2);
+Ymax=size(get(handles.ymax,'String'),2);
 
-%Grafica 2
-axes(handles.g_cu);
-ax = gca;   
-cla
-hold on
-mesh(Cx,Cy,Cz)
-if hObject.Value == 1
-    plot(Px,Py,'r.','MarkerSize',10)
-end
-if Cn == 1
-    fcontour(F_xy,[Xmin Xmax Ymin Ymax])
-end
+if F_xy ~= 0 && Xmin ~= 0 && Xmax ~= 0 && Ymin ~= 0 && Ymax~=0
+    Cn=get(handles.CB_C,'Value');
+    F_xy=str2sym(get(handles.Fxy,'String'));
+    Xmin=str2double(get(handles.xmin,'String'));
+    Xmax=str2double(get(handles.xmax,'String'));
+    Ymin=str2double(get(handles.ymin,'String'));
+    Ymax=str2double(get(handles.ymax,'String'));
+    xd=str2double(get(handles.xdiv,'String'));
+    yd=str2double(get(handles.ydiv,'String'));
+    Reg=str2double(get(handles.Reg,'String'));
+    Px=transpose(PPx);
+    Py=transpose(PPy);
+    Fxy(x,y)=F_xy;
 
-%Grafica 3
-axes(handles.g_zoom)
-ax = gca;   
-cla
-axis([Cc(Reg) Cc(Reg)+Dx Ff(Reg) Ff(Reg)+Dy]);
-hold on
-mesh(Cx,Cy,Cz)
-plot(Px,Py,'r.','MarkerSize',30)
-if Cn == 1
-    fcontour(F_xy,[Xmin Xmax Ymin Ymax])
+    %Grafica 1
+    axes(handles.g_fxy);
+    ax = gca;   
+    cla
+    grid on
+    hold on
+    mesh(Cx,Cy,Cz)
+    fmesh(F_xy,[Xmin Xmax Ymin Ymax])
+    if hObject.Value == 1
+        plot(Px,Py,'r.','MarkerSize',10)
+    end
+    if Cn == 1
+        fcontour(F_xy,[Xmin Xmax Ymin Ymax])
+    end
+
+    %Grafica 2
+    axes(handles.g_cu);
+    ax = gca;   
+    cla
+    hold on
+    mesh(Cx,Cy,Cz)
+    if hObject.Value == 1
+        plot(Px,Py,'r.','MarkerSize',10)
+    end
+    if Cn == 1
+        fcontour(F_xy,[Xmin Xmax Ymin Ymax])
+    end
+
+    %Grafica 3
+    axes(handles.g_zoom)
+    ax = gca;   
+    cla
+    axis([Cc(Reg) Cc(Reg)+Dx Ff(Reg) Ff(Reg)+Dy]);
+    hold on
+    mesh(Cx,Cy,Cz)
+    plot(Px,Py,'r.','MarkerSize',30)
+    if Cn == 1
+        fcontour(F_xy,[Xmin Xmax Ymin Ymax])
+    end
+else
+    disp("No hay datos");
 end
 
 
@@ -327,58 +381,70 @@ end
 function CB_C_Callback(hObject, eventdata, handles)
 global PPx PPy Dx Dy Cx Cy Cz Cc Ff
 syms x y
-P=get(handles.CB_P,'Value');
-F_xy=str2sym(get(handles.Fxy,'String'));
-Xmin=str2double(get(handles.xmin,'String'));
-Xmax=str2double(get(handles.xmax,'String'));
-Ymin=str2double(get(handles.ymin,'String'));
-Ymax=str2double(get(handles.ymax,'String'));
-xd=str2double(get(handles.xdiv,'String'));
-yd=str2double(get(handles.ydiv,'String'));
-Reg=str2double(get(handles.Reg,'String'));
-Px=transpose(PPx);
-Py=transpose(PPy);
-Fxy(x,y)=F_xy;
 
-%Grafica 1
-axes(handles.g_fxy);
-ax = gca;   
-cla
-grid on
-hold on
-mesh(Cx,Cy,Cz)
-fmesh(F_xy,[Xmin Xmax Ymin Ymax])
-if P == 1
-    plot(Px,Py,'r.','MarkerSize',10)
-end
-if hObject.Value == 1
-    fcontour(F_xy,[Xmin Xmax Ymin Ymax])
+F_xy=size(get(handles.Fxy,'String'),2);
+Xmin=size(get(handles.xmin,'String'),2);
+Xmax=size(get(handles.xmax,'String'),2);
+Ymin=size(get(handles.ymin,'String'),2);
+Ymax=size(get(handles.ymax,'String'),2);
+
+if F_xy ~= 0 && Xmin ~= 0 && Xmax ~= 0 && Ymin ~= 0 && Ymax~=0
+    P=get(handles.CB_P,'Value');
+    F_xy=str2sym(get(handles.Fxy,'String'));
+    Xmin=str2double(get(handles.xmin,'String'));
+    Xmax=str2double(get(handles.xmax,'String'));
+    Ymin=str2double(get(handles.ymin,'String'));
+    Ymax=str2double(get(handles.ymax,'String'));
+    xd=str2double(get(handles.xdiv,'String'));
+    yd=str2double(get(handles.ydiv,'String'));
+    Reg=str2double(get(handles.Reg,'String'));
+    Px=transpose(PPx);
+    Py=transpose(PPy);
+    Fxy(x,y)=F_xy;
+
+    %Grafica 1
+    axes(handles.g_fxy);
+    ax = gca;   
+    cla
+    grid on
+    hold on
+    mesh(Cx,Cy,Cz)
+    fmesh(F_xy,[Xmin Xmax Ymin Ymax])
+    if P == 1
+        plot(Px,Py,'r.','MarkerSize',10)
+    end
+    if hObject.Value == 1
+        fcontour(F_xy,[Xmin Xmax Ymin Ymax])
+    end
+
+    %Grafica 2
+    axes(handles.g_cu);
+    ax = gca;   
+    cla
+    hold on
+    mesh(Cx,Cy,Cz)
+    if P == 1
+        plot(Px,Py,'r.','MarkerSize',10)
+    end
+    if hObject.Value == 1
+        fcontour(F_xy,[Xmin Xmax Ymin Ymax])
+    end
+
+    %Grafica 3
+    axes(handles.g_zoom)
+    ax = gca;   
+    cla
+    axis([Cc(Reg) Cc(Reg)+Dx Ff(Reg) Ff(Reg)+Dy]);
+    hold on
+    mesh(Cx,Cy,Cz)
+    plot(Px,Py,'r.','MarkerSize',30)
+    if hObject.Value == 1
+        fcontour(F_xy,[Xmin Xmax Ymin Ymax])
+    end
+else
+    disp("No hay datos");
 end
 
-%Grafica 2
-axes(handles.g_cu);
-ax = gca;   
-cla
-hold on
-mesh(Cx,Cy,Cz)
-if P == 1
-    plot(Px,Py,'r.','MarkerSize',10)
-end
-if hObject.Value == 1
-    fcontour(F_xy,[Xmin Xmax Ymin Ymax])
-end
-
-%Grafica 3
-axes(handles.g_zoom)
-ax = gca;   
-cla
-axis([Cc(Reg) Cc(Reg)+Dx Ff(Reg) Ff(Reg)+Dy]);
-hold on
-mesh(Cx,Cy,Cz)
-plot(Px,Py,'r.','MarkerSize',30)
-if hObject.Value == 1
-    fcontour(F_xy,[Xmin Xmax Ymin Ymax])
-end
 
 function ymax_Callback(hObject, eventdata, handles)
 
